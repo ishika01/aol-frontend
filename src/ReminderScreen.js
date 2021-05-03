144833d1ae01c31e1a2ad1df9509e2ee69799b29
Aditya1405 <adityaprakash0598@gmail.com>
2021-05-03 23:05:49 +0530

    commit-3
    date working and printing

--- src/ReminderScreen.js
+++ src/ReminderScreen.js
@@ -1,21 +1,57 @@
 import React, { useState } from 'react'
-import {View,StyleSheet,Text} from 'react-native'
-import DatePicker from 'react-native-date-picker'
+import { Button, View, StyleSheet,Text } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const ReminderScreen = function(){
-    const [date, setDate] = useState(new Date())
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+    const [cdate,setDate]=useState('')
+    const showDatePicker = () => {
+    setDatePickerVisibility(true);
+    };
+
+    const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+    };
+
+    const handleConfirm = (date) => {
+        const d =date.toString()
+        console.warn("A date has been picked: ", d);
+        setDate(d);
+        hideDatePicker();
+    };
+    console.log(cdate)
+    console.log("=========================================")
     return (
-        <View>
-            <Text>this rem screen</Text>
-            <DatePicker
-                date={date}
-                onDateChange={setDate}
-            />
-        </View>
-    )
+    <View>
+        <Button title="Show Date Picker" onPress={showDatePicker} />
+        <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+        />
+        <Text>{cdate}</Text>
+    </View>
+    );
+
 }
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      title: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding: 20,
+      },
+      datePickerStyle: {
+        width: 200,
+        marginTop: 20,
+      },
 })
 
 export default ReminderScreen 
