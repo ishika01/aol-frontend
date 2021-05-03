8f5a42053a219ad7f665496388f246b36c8174f7
Aditya1405 <adityaprakash0598@gmail.com>
2021-05-03 08:14:03 +0530

    contact imports are working

--- android/app/src/main/java/com/myapp/MainApplication.java
+++ android/app/src/main/java/com/myapp/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts; // <--- import
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -21,7 +22,8 @@ public class MainApplication extends Application implements ReactApplication {
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
         }
-
+        
+       
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
--- src/ContactScreen.js
+++ src/ContactScreen.js
@@ -1,6 +1,25 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+
+// Import all required component
+import {
+  PermissionsAndroid,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+} from 'react-native';
+
+import Contacts from 'react-native-contacts';
 const ContactScreen = function(){
+
+  Contacts.getAll().then(contacts => {
+    // contacts returned
+    console.log("hello==========================================")
+    console.log(contacts.length)
+  })
     return(
         <View style={style.container}>
             <Text>
@@ -19,3 +38,4 @@ const style = StyleSheet.create({
 })
 
 export default ContactScreen;
+
