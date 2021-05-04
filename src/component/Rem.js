bc54cb18699820a459c3506f64bb53aa1dfdea52
Aditya1405 <adityaprakash0598@gmail.com>
2021-05-04 11:42:39 +0530

    Revert "commit before pull"
    This reverts commit 83b90bdb8578210a1a7ff1b690b798980fd0bac2.

--- src/component/Rem.js
+++ src/component/Rem.js
@@ -1,89 +1,86 @@
-import React, { useState, useEffect } from 'react'
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
-import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import React, { useState,useEffect } from 'react'
+import { Button, View, StyleSheet,Text, TouchableOpacity } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Rem = function (props) {
-    //const [dateswitch ,setDateSwitch]=useState(true)
+const Rem = function(props){
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-    const [cdate, setDate] = useState('')
-    //const [c2date,setc2Date]=useState('')
-    //to store asyncstorage date
-    const [prevdate, setprevdate] = useState('')
+    const [cdate,setDate]=useState('')
+    const [prevdate,setprevdate]=useState('')
     //================================
-    useEffect(async () => {
+    useEffect( async () => {
         try {
             const value = await AsyncStorage.getItem('date')
-            if (value !== null) {
-                // value previously stored
+            if(value !== null) {
+            // value previously stored
                 setprevdate(value)
             }
-        } catch (e) {
+        } catch(e) {
             // error reading value
         }
-    }, [])
+    },[])
     //================================
     const showDatePicker = () => {
-        setDatePickerVisibility(true);
+    setDatePickerVisibility(true);
     };
 
     const hideDatePicker = () => {
-        setDatePickerVisibility(false);
+    setDatePickerVisibility(false);
     };
 
     const handleConfirm = async (date) => {
-        try {
-            const d = date.toString()
-            console.warn('A date has been picked: ', d);
-            await AsyncStorage.setItem('date', d);
+        try{
+            const d =date.toString()
+            console.warn("A date has been picked: ", d);
+            await AsyncStorage.setItem('date',d)
             setDate(d);
             hideDatePicker();
-        } catch (err) {
+        }catch(err){
             console.log(err)
         }
     };
-
-
-
-
+    
+    
+    
+  
 
 
     //console.log(cdate)
     //console.log("=========================================")
     return (
-        <View style={styles.container}>
-            <TouchableOpacity onPress={showDatePicker}>
-                <Text style={styles.button}>{props.title}</Text>
-                <Text style={styles.button}>{prevdate}</Text>
-            </TouchableOpacity>
-            <Text style={styles.text}>{cdate}</Text>
-            <DateTimePickerModal
-                isVisible={isDatePickerVisible}
-                mode="date"
-                onConfirm={handleConfirm}
-                onCancel={hideDatePicker}
-            />
-        </View>
+    <View style={styles.container}>
+        <TouchableOpacity onPress={showDatePicker}>
+            <Text style={styles.button}>{props.title}</Text>
+            <Text style={styles.button}>{prevdate}</Text>
+        </TouchableOpacity>
+        <Text style={styles.text}>{cdate}</Text>
+        <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+        />
+    </View>
     );
 
 }
 const styles = StyleSheet.create({
-    text: { fontSize: 24 },
+    text:{fontSize:24},
     button: {
-        fontSize: 18,
+        fontSize:18,
         paddingHorizontal: 8,
         paddingVertical: 6,
         borderRadius: 4,
         //alignSelf: 'flex-start',
         marginHorizontal: '1%',
         marginBottom: 6,
-        backgroundColor: '#3483eb',
-        width: 150,
-        height: 40,
-        color: 'white',
+        backgroundColor:'#3483eb',
+        width:150,
+        height:40,
+        color:'white',
         textAlign: 'center',
-    },
-
+      },
+     
 })
 
 export default Rem
