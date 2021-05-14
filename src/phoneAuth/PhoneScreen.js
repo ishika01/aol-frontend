3c0fa13f02e7c315c58988922a6f25af3344692f
Aditya1405 <adityaprakash0598@gmail.com>
2021-05-14 20:26:58 +0530

    commit  f1

--- src/phoneAuth/PhoneScreen.js
+++ src/phoneAuth/PhoneScreen.js
@@ -1,63 +1,45 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Pressable, TextInput } from 'react-native';
-//import smurfImg from '../../../assets/smurf.png';
+import { Button, TextInput } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
-const PhoneScreen = ({ navigation }) => {
-    const [phoneNumber, addPhoneNumber] = useState('+91');
+const PhoneScreen = () => {
+    // If null, no SMS has been sent
+    const [confirm, setConfirm] = useState(null);
 
-    const GetOTP = () => {
-        console.log('get otp');
-        {/*if(phoneNumber && phoneNumber.length > 9){
-        navigation.navigate('OTP Screen', {phoneNumber});
+    const [code, setCode] = useState('');
+
+    // Handle the button press
+    async function signInWithPhoneNumber(phoneNumber) {
+        const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+        setConfirm(confirmation);
+    }
+
+    async function confirmCode() {
+        console.log('working');
+        try {
+            await confirm.confirm(code);
+        } catch (error) {
+            console.log('Invalid code.');
         }
-        else
-    alert("Please enter 10 digit phone number");
-*/}
+    }
 
+    if (!confirm) {
+        return (
+            <Button
+                title="Phone Number Sign In"
+                onPress={() => {
+                    console.log('hello');
+                    signInWithPhoneNumber('+44 7444 555666');
+                }}
+            />
+        );
     }
 
     return (
-        <View style={styles.container}>
-            <TextInput
-                value={phoneNumber}
-                style={{ height: 50, width: 300, paddingLeft: 20, backgroundColor: 'azure', fontSize: 20 }}
-                placeholder="+91 1234567890"
-                onChangeText={(text) => addPhoneNumber(text)}
-            />
-            <Pressable onPress={GetOTP}
-                style={({ pressed }) => ({ ...styles.btnContainer, backgroundColor: pressed ? 'white' : 'blue' })}>
-                <Text style={styles.btnText}>
-                    Get OTP!
-                </Text>
-            </Pressable>
-        </View>
+        <>
+            <TextInput value={code} onChangeText={text => setCode(text)} />
+            <Button title="Confirm Code" onPress={() => confirmCode()} />
+        </>
     );
-};
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    btnContainer: {
-        padding: 20,
-        borderRadius: 10,
-        marginTop: 30,
-    },
-    btnText: {
-        color: 'white',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    imageStyle: {
-        height: 200, width: 150,
-        resizeMode: 'contain',
-        marginBottom: 50,
-    },
-    text: {
-        color: 'black',
-        fontSize: 20,
-    },
-});
-
+}
 export default PhoneScreen;
