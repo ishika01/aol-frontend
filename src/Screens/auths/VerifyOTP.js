84071cdc732f1d0e77c71298c244cd10800923f1
Aditya1405 <adityaprakash0598@gmail.com>
2021-05-15 10:00:33 +0530

    commit login ui
    copied components from oreo
    created login pages
    removed server folder

---
+++ src/Screens/auths/VerifyOTP.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { TextInput, View, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+const VerifyOTP = () => {
+    // If null, no SMS has been sent
+    //const [confirm, setConfirm] = useState(true);
+    const [otp, setOtp] = useState('');
+
+    return (
+        <View>
+            <TextInput
+                style={style.display}
+                value={otp}
+                onChangeText={text => setOtp(text)}
+                keyboardType="number-pad"
+            />
+            <TouchableOpacity
+                onPress={() => {
+                    console.log('works');
+                }}
+                onLongPress={() => { console.log('long'); }}
+            >
+                <Text>
+                    submit
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    display: {
+        borderColor: 'black',
+        borderWidth: 2,
+    }
+});
+export default VerifyOTP;
