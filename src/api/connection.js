606dec5752604ac24167e0d7b7ee8ddd6491505f
Aditya1405 <adityaprakash0598@gmail.com>
2021-05-23 11:52:04 +0530

    commit signin/sigup fxnl

--- src/api/connection.js
+++ src/api/connection.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL: 'http://9e3ffebbbfc0.ngrok.io',
+    baseURL: 'http://1fd02b7db0c7.ngrok.io',
 });
 
 export default instance;
