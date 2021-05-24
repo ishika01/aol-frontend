116ca84410a66c591825c8d61ff21c7abf9964ee
Aditya1405 <adityaprakash0598@gmail.com>
2021-05-24 23:06:23 +0530

    commit balancing changes btw test & crs

--- src/CreateCourse.js
+++ src/CreateCourse.js
@@ -1,7 +1,14 @@
-
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity,StatusBar } from 'react-native';
-import data from './data';
+import React, { useState, useRef } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  FlatList,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+import { FAB, TextInput, Card } from 'react-native-paper';
 import { exp, Transition, Transitioning } from 'react-native-reanimated';
 
 const transition = (
@@ -11,72 +18,162 @@ const transition = (
     <Transition.Out type="fade" durationMs={200} />
   </Transition.Together>
 );
+const CreateCourse = () => {
+  const [course, setCourse] = useState([]);
+  const [fabstate, setFabstate] = useState(0);
+  const [index, setIndex] = useState(0);
+  const [C_Name, setC_Name] = useState('');
+  const [C_Id, setC_Id] = useState('');
+  const [C_Add, setC_Add] = useState('');
 
-const CreateCourse = ()=>{
-  const [currentIndex, setCurrentIndex] = React.useState(null);
-  const ref = React.useRef();
+  const [currentIndex, setCurrentIndex] = useState(null);
+  const ref = useRef();
+  //========================================================
+  console.log(fabstate);
 
+  const submitcourse = () => {
+    const ind = course.length + 1;
+    console.log('ind == > ', ind);
+    setFabstate(0);
+    const arr = {
+      Cname: C_Name,
+      Cid: C_Id,
+      Cadd: C_Add,
+      index: ind,
+    };
+    setCourse([...course, arr]);
+  };
+  //========================================================
+  console.log(course);
   return (
-    <Transitioning.View
-      ref={ref}
-      transition={transition}
-      style={styles.container}
-    >
-
-      {data.map(({ bg, color, category, subCategories }, index) => {
-        return (
-          <TouchableOpacity
-            key={category}
-            onPress={() => {
-              ref.current.animateNextTransition();
-              setCurrentIndex(index === currentIndex ? null : index);
-            }}
-            style={styles.cardContainer}
-            activeOpacity={0.9}
-          >
-            <View style={[styles.card, { backgroundColor: bg }]}>
-              <Text style={[styles.heading, { color }]}>{category}</Text>
-              {index === currentIndex && (
-                <View style={styles.subCategoriesList}>
-                  {subCategories.map((subCategory) => (
-                    <Text key={subCategory} style={[styles.body, { color }]}>
-                      {subCategory}
-                    </Text>
-                  ))}
-                </View>
-              )}
-            </View>
-          </TouchableOpacity>
-        );
-      })}
-    </Transitioning.View>
+    <View style={{ borderWidth: 2, borderColor: 'black', flex: 10 }}>
+      {/*add a + bar*/}
+      {fabstate === 0 ? (
+        <View style={style.container}>
+          <View style={{ borderWidth: 2, borderColor: 'black', flex: 10 }}>
+            <Transitioning.View
+              ref={ref}
+              transition={transition}
+              style={style.container}>
+              <FlatList
+                data={course}
+                keyExtractor={(item) => item.index}
+                renderItem={({ item }) => {
+                  return (
+                    <TouchableOpacity
+                      style={style.cardContainer}
+                      activeOpacity={0.9}
+                      onPress={() => {
+                        console.log(item.index);
+                        ref.current.animateNextTransition();
+                        setCurrentIndex(
+                          item.index === currentIndex ? null : item.index
+                        );
+                      }}>
+                      {/*<Text style={style.title}>
+                            {item.Cname}=={item.index}
+                        </Text>*/}
+                      <View style={style.card}>
+                        <Text>{item.Cname}</Text>
+                        {item.index === currentIndex && (
+                          <View style={style.subCategoriesList}>
+                            <Text>
+                              {item.index}=={item.Cname}
+                            </Text>
+                          </View>
+                        )}
+                      </View>
+                    </TouchableOpacity>
+                  );
+                }}
+              />
+            </Transitioning.View>
+          </View>
+          <View style={{ borderWidth: 2, borderColor: 'black', flex: 2 }}>
+            <FAB style={style.fab} icon="plus" onPress={() => setFabstate(1)} />
+          </View>
+        </View>
+      ) : null}
+      {fabstate === 1 ? (
+        <View>
+          {/** display a card with a submit button*/}
+          <Text>Create New Course</Text>
+          <Card>
+            <TextInput
+              label="Course Name"
+              mode="outlined"
+              value={C_Name}
+              onChangeText={setC_Name}
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+            <TextInput
+              mode="outlined"
+              label="CourseId"
+              value={C_Id}
+              onChangeText={setC_Id}
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+            <TextInput
+              mode="outlined"
+              label="VenueAddress"
+              value={C_Add}
+              onChangeText={setC_Add}
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+            <Button
+              title="submit"
+              onPress={() => {
+                setIndex(index + 1);
+                submitcourse();
+              }}
+            />
+            <Button
+              title="cancel"
+              onPress={() => {
+                setFabstate(0);
+              }}
+            />
+          </Card>
+        </View>
+      ) : null}
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
+const style = StyleSheet.create({
   cardContainer: {
     flexGrow: 1,
+  },
+  touchstyle: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: 'white',
+    fontSize: 18,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  fab: {
+    borderWidth: 5,
+    position: 'absolute',
+    margin: 16,
+    right: 0,
   },
   card: {
     flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  heading: {
-    fontSize: 38,
-    fontWeight: '900',
-    textTransform: 'uppercase',
-    letterSpacing: -2,
-  },
-  body: {
-    fontSize: 20,
-    lineHeight: 20 * 1.5,
-    textAlign: 'center',
+    borderWidth: 2,
+    marginBottom: 10,
   },
   subCategoriesList: {
     marginTop: 20,
--- src/Test.js
+++ src/Test.js
@@ -1,200 +1,86 @@
-import React, { useState,useRef } from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  Button,
-  FlatList,
-  StatusBar,
-  TouchableOpacity,
-} from 'react-native';
-import { FAB, TextInput, Card } from 'react-native-paper';
+
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,StatusBar } from 'react-native';
+import data from './data';
 import { exp, Transition, Transitioning } from 'react-native-reanimated';
 
 const transition = (
-    <Transition.Together>
-      <Transition.In type="fade" durationMs={200} />
-      <Transition.Change />
-      <Transition.Out type="fade" durationMs={200} />
-    </Transition.Together>
-  );
-const CreateCourse = () => {
-  const initialValue = [
-    {
-      Cname: '',
-      Cid: '',
-      Cadd: '',
-      index: 0,
-    },
-  ];
-  const [course, setCourse] = useState(initialValue);
-  const [fabstate, setFabstate] = useState(0);
-  const [index, setIndex] = useState(0);
-  const [C_Name, setC_Name] = useState('');
-  const [C_Id, setC_Id] = useState('');
-  const [C_Add, setC_Add] = useState('');
+  <Transition.Together>
+    <Transition.In type="fade" durationMs={200} />
+    <Transition.Change />
+    <Transition.Out type="fade" durationMs={200} />
+  </Transition.Together>
+);
 
+const Test = ()=>{
+  const [currentIndex, setCurrentIndex] = React.useState(null);
+  const ref = React.useRef();
 
-  const [currentIndex, setCurrentIndex] = useState(null);
-  const ref = useRef();
-  //========================================================
-  console.log(fabstate);
-
-  const submitcourse = () => {
-    const ind = course.length;
-    console.log('ind == > ', ind);
-    setFabstate(0);
-    const arr = {
-      Cname: C_Name,
-      Cid: C_Id,
-      Cadd: C_Add,
-      index: ind,
-    };
-    setCourse([...course, arr]);
-  };
-  //========================================================
-  console.log(course);
   return (
-    <View  style={{ borderWidth: 2, borderColor: 'black', flex: 10 }}>
-      {/*add a + bar*/}
-      {fabstate === 0 ? (
-
-        <View style={style.container}>
-          <View style={{ borderWidth: 2, borderColor: 'black', flex: 10 }}>
-          <Transitioning.View
+    <Transitioning.View
       ref={ref}
       transition={transition}
-      style={style.container}
-     >
-            <FlatList
-                data={course}
-                keyExtractor={item => item.index}
-                renderItem={({ item }) => {
-                return (
-                    <TouchableOpacity
-                        style={style.touchstyle}
-                        activeOpacity={0.9}
-                        onPress={()=>{
-                            console.log(item.index);
-                            ref.current.animateNextTransition();
-                            setCurrentIndex(item.index === currentIndex ? null : item.index);
+      style={styles.container}
+    >
 
-                            }}
-                        >
-                        {/*<Text style={style.title}>
-                            {item.Cname}=={item.index}
-                        </Text>*/}
-                        <View style={style.card}>
-              <Text>{item.Cname}</Text>
-              {item.index === currentIndex && (
-                <View style={style.subCategoriesList}>
-                  <Text>{item.index}=={item.Cname}</Text>
+      {data.map(({ bg, color, category, subCategories }, index) => {
+        return (
+          <TouchableOpacity
+            key={category}
+            onPress={() => {
+              ref.current.animateNextTransition();
+              setCurrentIndex(index === currentIndex ? null : index);
+            }}
+            style={styles.cardContainer}
+            activeOpacity={0.9}
+          >
+            <View style={[styles.card, { backgroundColor: bg }]}>
+              <Text style={[styles.heading, { color }]}>{category}</Text>
+              {index === currentIndex && (
+                <View style={styles.subCategoriesList}>
+                  {subCategories.map((subCategory) => (
+                    <Text key={subCategory} style={[styles.body, { color }]}>
+                      {subCategory}
+                    </Text>
+                  ))}
                 </View>
               )}
             </View>
-                    </TouchableOpacity>
-                );
-                }}
-                />
-                </Transitioning.View>
-          </View>
-          <View style={{borderWidth: 2, borderColor: 'black', flex: 2 }}>
-            <FAB
-                style={style.fab}
-                icon="plus"
-                onPress={() => setFabstate(1)}
-            />
-          </View>
-        </View>
-      ) : null}
-      {fabstate === 1 ? (
-        <View>
-          {/** display a card with a submit button*/}
-          <Text>Create New Course</Text>
-          <Card>
-            <TextInput
-              label="Course Name"
-              mode="outlined"
-              value={C_Name}
-              onChangeText={setC_Name}
-              autoCapitalize="none"
-              autoCorrect={false}
-            />
-            <TextInput
-              mode="outlined"
-              label="CourseId"
-              value={C_Id}
-              onChangeText={setC_Id}
-              autoCapitalize="none"
-              autoCorrect={false}
-            />
-            <TextInput
-              mode="outlined"
-              label="VenueAddress"
-              value={C_Add}
-              onChangeText={setC_Add}
-              autoCapitalize="none"
-              autoCorrect={false}
-            />
-            <Button
-              title="submit"
-              onPress={() => {
-                setIndex(index + 1);
-                submitcourse();
-              }}
-            />
-            <Button
-              title="cancel"
-              onPress={() => {
-                setFabstate(0);
-              }}
-            />
-          </Card>
-        </View>
-      ) : null}
-    </View>
+          </TouchableOpacity>
+        );
+      })}
+    </Transitioning.View>
   );
 };
 
-const style = StyleSheet.create({
-  touchstyle: {
-    marginTop: '5%',
-    marginLeft: '10%',
-    marginRight: '10%',
-    height: 50,
-    width: '80%',
-    borderRadius: 4,
-    backgroundColor: 'green',
-    justifyContent: 'space-between',
-    paddingHorizontal: 25,
-    flexDirection: 'row',
-    alignItems: 'center',
-    flexGrow: 1,
-  },
-  title: {
-    color: 'white',
-    fontSize: 18,
-  },
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
   },
-  fab: {
-    borderWidth: 5,
-    position: 'absolute',
-    margin: 16,
-    right: 0,
+  cardContainer: {
+    flexGrow: 1,
   },
   card: {
     flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    fontSize: 38,
+    fontWeight: '900',
+    textTransform: 'uppercase',
+    letterSpacing: -2,
+  },
+  body: {
+    fontSize: 20,
+    lineHeight: 20 * 1.5,
+    textAlign: 'center',
+  },
   subCategoriesList: {
     marginTop: 20,
   },
 });
 
-export default CreateCourse;
+export default Test;
